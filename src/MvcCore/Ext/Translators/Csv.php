9ee6b98437fe85e810cba84612c7eb7a904adb3f
Tom Flídr <tomflidr@gmail.com>
2024-12-16 14:04:59 +0100

    release v5.3.1

--- src/MvcCore/Ext/Translators/Csv.php
+++ src/MvcCore/Ext/Translators/Csv.php
@@ -30,7 +30,7 @@ implements	\MvcCore\Ext\ITranslator {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.3.0';
+	const VERSION = '5.3.1';
 
 	/**
 	 * Relative path to directory with CSV translations, 
